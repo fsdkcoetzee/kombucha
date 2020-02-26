c49c9fa042fe82044d0cb83400e93704980ffd72
fsdkcoetzee <61516760+fsdkcoetzee@users.noreply.github.com>
2020-02-26 20:43:24 +0200

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 26 20:12:39 2020
+
+@author: fsdkc
+"""
+
+def addition (number_1, number_2):
+    
+    summation = number_1 + number_2
+    
+    return summation
+#
---
+++ math.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+
+"""
+'/n'
+import functions
+
+#a = int(input("Please provide a number:   "))
+#b = int(input("Please provide another number:   "))
+
+a=10
+b=20
+
+output = functions.addition(a,b)
+
+print(f"The answer of the sum is: {output}")
